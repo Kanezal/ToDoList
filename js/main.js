2747a45df10697d1aff7a41bf834887e3650f531
Kanezal <Kanezal@yandex.ru>
2022-05-27 00:28:21 +0300

    Прости меня, Макс, что я снес весь твой код

--- js/main.js
+++ js/main.js
@@ -7,54 +7,57 @@ const list = document.querySelector(".list");
 let Tasks = [];
 
 btnAdd.addEventListener("click", (e) => {
-    let taskName = prompt("Введите задачу:");
-
     // Создание нового таска
     let newTask = document.createElement("div");
     newTask.classList.add("task");
+    newTask.className += " input-group mt-3"
     Tasks.push(newTask);
 
     let taskIndex = Tasks.indexOf(newTask);
-    newTask.innerHTML = `<input type="checkbox" class="chb" id="chb-done${taskIndex}">
-    <label for="chb-done${taskIndex}"><span class="checkbox checkbox${taskIndex}">${taskName}</span></label>`;
+    newTask.innerHTML = `
+    <div class="input-group-text pe-5">
+        <input class="form-check-input ms-2" type="checkbox" style="width: 1.5rem; height: 1.5rem" value="" id="chb-done${taskIndex}">
+    </div>
+    <textarea class="form-control" placeholder="Добавьте дело, которое хотите сделать" id="floatingTextarea"></textarea>
+    `;
     
     list.appendChild(newTask);
 
-    const taskEls = document.querySelectorAll(".task");
-    taskEls.forEach(el => createCheckboxPosForTask(el));
+    // const taskEls = document.querySelectorAll(".task");
+    // taskEls.forEach(el => createCheckboxPosForTask(el));
 });
 
 
 
 
-// Функция для подсчета положения checkbox для каждого task
-function countCheckboxPos(el){
-    // Доберемся до элемента span.checkbox
-    const spanCheckbox = el.lastChild.firstChild;
-    console.log(el.lastChild);
-    console.log(el.lastChild.previousSibling)
-    console.log(spanCheckbox);
-    // Чуть магии для получения стилей padding
-    let isStyle = el.currentStyle || window.getComputedStyle(el, null);
+// // Функция для подсчета положения checkbox для каждого task
+// function countCheckboxPos(el){
+//     // Доберемся до элемента span.checkbox
+//     const spanCheckbox = el.lastChild.firstChild;
+//     console.log(el.lastChild);
+//     console.log(el.lastChild.previousSibling)
+//     console.log(spanCheckbox);
+//     // Чуть магии для получения стилей padding
+//     let isStyle = el.currentStyle || window.getComputedStyle(el, null);
 
-    let s = el.clientWidth - parseInt(isStyle.paddingLeft) * 2 - spanCheckbox.offsetWidth;
-    return -s;
-}
+//     let s = el.clientWidth - parseInt(isStyle.paddingLeft) * 2 - spanCheckbox.offsetWidth;
+//     return -s;
+// }
 
 
-function createCheckboxPosForTask(el){
-    // Идея - добавить style тег в head, чтобы там описать ::before ::after;
-    let styleEl;
-    let taskIndex = Tasks.indexOf(el);
-    const head = document.querySelector("head");
-    styleEl = document.createElement("style");
-    styleEl.innerHTML = 
-    `span.checkbox${taskIndex}::before { 
-        left: ${countCheckboxPos(el)}px;
-    } \n` + 
-    `span.checkbox${taskIndex}::after {
-        left: ${countCheckboxPos(el) + 6}px;
-    } `; 
-    head.appendChild(styleEl);
-}
+// function createCheckboxPosForTask(el){
+//     // Идея - добавить style тег в head, чтобы там описать ::before ::after;
+//     let styleEl;
+//     let taskIndex = Tasks.indexOf(el);
+//     const head = document.querySelector("head");
+//     styleEl = document.createElement("style");
+//     styleEl.innerHTML = 
+//     `span.checkbox${taskIndex}::before { 
+//         left: ${countCheckboxPos(el)}px;
+//     } \n` + 
+//     `span.checkbox${taskIndex}::after {
+//         left: ${countCheckboxPos(el) + 6}px;
+//     } `; 
+//     head.appendChild(styleEl);
+// }
 
